34cd383778729d8ebc8600f178879c555eca63e8
mharrvic <mharrvic@gmail.com>
2025-03-08 22:19:34 +0800

    Update env

--- modules/samples/app_settings.py
+++ modules/samples/app_settings.py
@@ -4,15 +4,17 @@ class AppSettings:
 
     Attributes:
         resource_group_name: The name of the resource group.
-        managed_identity_client_id: The client ID of the managed identity.
         storage_account_name: The name of the storage account.
-        ai_services_endpoint: The endpoint for the AI services.
-        openai_endpoint: The endpoint for the OpenAI service.
         gpt4o_model_deployment_name: The name of the GPT-4o model deployment.
         gpt4o_mini_model_deployment_name: The name of the GPT-4o Mini model deployment.
         text_embedding_model_deployment_name: The name of the text embedding model deployment.
-        phi_endpoint: The endpoint for the Phi deployment.
-        phi_primary_key: The primary key for the Phi deployment.
+        azure_openai_api_key: The API key for the Azure OpenAI service.
+        azure_openai_endpoint: The endpoint for the Azure OpenAI service.
+        azure_openai_region: The region for the Azure OpenAI service.
+        azure_openai_deployment_name: The deployment name for the Azure OpenAI service.
+        document_intelligence_api_key: The API key for the Document Intelligence service.
+        document_intelligence_endpoint: The endpoint for the Document Intelligence service.
+        document_intelligence_region: The region for the Document Intelligence service.
     """
 
     def __init__(self, config: dict):
@@ -20,16 +22,22 @@ class AppSettings:
         Initializes a new instance of the AppSettings class.
 
         Args:
-            config (dict): The environment configuration settings.        
+            config (dict): The environment configuration settings.
         """
 
-        self.resource_group_name = config['RESOURCE_GROUP_NAME']
-        self.managed_identity_client_id = config['MANAGED_IDENTITY_CLIENT_ID']
-        self.storage_account_name = config['STORAGE_ACCOUNT_NAME']
-        self.ai_services_endpoint = config['AI_SERVICES_ENDPOINT']
-        self.openai_endpoint = config['OPENAI_ENDPOINT']
-        self.gpt4o_model_deployment_name = config['GPT4O_MODEL_DEPLOYMENT_NAME']
-        self.gpt4o_mini_model_deployment_name = config['GPT4O_MINI_MODEL_DEPLOYMENT_NAME']
-        self.text_embedding_model_deployment_name = config['TEXT_EMBEDDING_MODEL_DEPLOYMENT_NAME']
-        self.phi_endpoint = config['PHI_ENDPOINT']
-        self.phi_primary_key = config['PHI_PRIMARY_KEY']
+        self.resource_group_name = config["RESOURCE_GROUP_NAME"]
+        self.storage_account_name = config["STORAGE_ACCOUNT_NAME"]
+        self.gpt4o_model_deployment_name = config["GPT4O_MODEL_DEPLOYMENT_NAME"]
+        self.gpt4o_mini_model_deployment_name = config[
+            "GPT4O_MINI_MODEL_DEPLOYMENT_NAME"
+        ]
+        self.text_embedding_model_deployment_name = config[
+            "TEXT_EMBEDDING_MODEL_DEPLOYMENT_NAME"
+        ]
+        self.azure_openai_api_key = config["AZURE_OPENAI_API_KEY"]
+        self.azure_openai_endpoint = config["AZURE_OPENAI_ENDPOINT"]
+        self.azure_openai_region = config["AZURE_OPENAI_REGION"]
+        self.azure_openai_deployment_name = config["AZURE_OPENAI_DEPLOYMENT_NAME"]
+        self.document_intelligence_api_key = config["DOCUMENT_INTELLIGENCE_API_KEY"]
+        self.document_intelligence_endpoint = config["DOCUMENT_INTELLIGENCE_ENDPOINT"]
+        self.document_intelligence_region = config["DOCUMENT_INTELLIGENCE_REGION"]
