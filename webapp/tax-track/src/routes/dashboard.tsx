be86dae7d03b7c0f22ba8779be6e2ac9ff510c7c
mharrvic <mharrvic@gmail.com>
2026-03-04 21:42:24 +0800

    Move validated panel above cards on dashboard

--- webapp/tax-track/src/routes/dashboard.tsx
+++ webapp/tax-track/src/routes/dashboard.tsx
@@ -55,16 +55,16 @@ function RouteComponent() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
-              <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
-              </div>
               <div className="px-4 lg:px-6">
                 <ValidatedDocumentsPanel
                   search={search}
                   onSearchChange={updateSearch}
                   controlPlacement="top-right"
                 />
+              </div>
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
               </div>
               <DataTable data={tableData} />
             </div>
