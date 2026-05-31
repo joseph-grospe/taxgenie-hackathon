1480318d6874347dd242261014aba5d9649d1879
joseph-grospe <joseph.grospe080698@gmail.com>
2026-05-31 21:49:28 +0800

    feat(infra): schedule batch retention purge

--- backend/infra/sst-env.d.ts
+++ backend/infra/sst-env.d.ts
@@ -10,11 +10,15 @@ declare module "sst" {
       "type": "sst.aws.TanstackStart"
       "url": string
     }
-    "taxtrack-dev-db-migrate": {
+    "taxtrack-uat-batch-retention": {
       "name": string
       "type": "sst.aws.Function"
     }
-    "taxtrack-dev-postgres-rds": {
+    "taxtrack-uat-db-migrate": {
+      "name": string
+      "type": "sst.aws.Function"
+    }
+    "taxtrack-uat-postgres-rds": {
       "database": string
       "host": string
       "password": string
@@ -22,7 +26,7 @@ declare module "sst" {
       "type": "sst.aws.Postgres"
       "username": string
     }
-    "taxtrack-dev-power-schedule": {
+    "taxtrack-uat-power-schedule": {
       "name": string
       "type": "sst.aws.Function"
     }
